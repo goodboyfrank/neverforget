2c03bea328f4d434270ff575cee2f74c01a9e7c8
zhangliuqing <sh94666@163.com>
2022-07-16 16:48:17 +0800

    test push

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button
 
+} from '@chakra-ui/react'
 export default function Home() {
-  return <div>Home</div>;
+  return <Menu>
+  <MenuButton as={Button} >
+    Actions
+  </MenuButton>
+  <MenuList>
+    <MenuItem>Download</MenuItem>
+    <MenuItem>Create a Copy</MenuItem>
+    <MenuItem>Mark</MenuItem>
+    <MenuItem>Delete</MenuItem>
+    <MenuItem>Attend a Workshop</MenuItem>
+  </MenuList>
+</Menu>
 }
